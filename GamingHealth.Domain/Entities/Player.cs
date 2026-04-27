588040704bcb8d85797aa75adbd91cbb5a36c7e1
Rafazg <claudenirorafaelbjj@gmail.com>
2026-04-27 02:14:50 -0300

    feat:(Domain) Added validation for updateIncome.

--- GamingHealth.Domain/Entities/Player.cs
+++ GamingHealth.Domain/Entities/Player.cs
@@ -11,7 +11,7 @@ namespace GamingHealth.Domain.Entities
         public int PlayerId { get; set; }
         public int? Age { get; set; }
         public string? Gender { get; set; }
-        public decimal? Income { get; set; }
+        public decimal Income { get; private set; }
         public decimal? Bmi { get; set; }
 
         // Navegação para as outras tabelas
@@ -20,5 +20,30 @@ namespace GamingHealth.Domain.Entities
         public Social? Social { get; set; }
         public Performance? Performance { get; set; }
         public FinancialTech? FinancialTech { get; set; }
+
+
+        //Construtor vazio
+        public Player()
+        {
+
+        }
+
+        //Construtor para Classe Player
+        public Player(int playerID, int? age, string? gender)
+        {
+            Console.WriteLine($"PlayerID: {playerID}, Age: {age}, Gender: {gender}");
+        }
+
+        public void UpdateIncome(decimal newIncome)
+        {
+            if (newIncome < 0)
+                throw new ArgumentException("Income cannot be negative.");
+            Income = newIncome;
+        }
+
+        public bool IsAdult() 
+        {
+            return (Age.HasValue && Age.Value >= 18);
+        }
     }
 }
