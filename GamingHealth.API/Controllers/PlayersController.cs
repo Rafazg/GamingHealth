0a488bb62f92c5175b45c6883525d6d0fbb3f850
Rafazg <claudenirorafaelbjj@gmail.com>
2026-04-27 02:11:31 -0300

    feat(API) Added PATCH endpoint to update income

--- GamingHealth.API/Controllers/PlayersController.cs
+++ GamingHealth.API/Controllers/PlayersController.cs
@@ -1,5 +1,8 @@
-﻿using GamingHealth.Application.UseCases.GetPlayerById;
+﻿using GamingHealth.API.Requests;
+using GamingHealth.Application.UseCases.GetPlayerById;
 using GamingHealth.Application.UseCases.ListPlayers;
+using GamingHealth.Application.UseCases.UpdatePlayerIncome;
+using GamingHealth.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GamingHealth.API.Controllers
@@ -10,13 +13,16 @@ namespace GamingHealth.API.Controllers
     {
         private readonly ListPlayersUseCase _listPlayersUseCase;
         private readonly GetPlayerByIdUseCase _getPlayerByIdUseCase;
+        private readonly UpdatePlayerIncomeUseCase _updatePlayerIncomeUseCase;
 
         public PlayersController(
             ListPlayersUseCase listPlayersUseCase,
-            GetPlayerByIdUseCase getPlayerByIdUseCase)
+            GetPlayerByIdUseCase getPlayerByIdUseCase,
+            UpdatePlayerIncomeUseCase updatePlayerIncomeUseCase)
         {
             _listPlayersUseCase = listPlayersUseCase;
             _getPlayerByIdUseCase = getPlayerByIdUseCase;
+            _updatePlayerIncomeUseCase = updatePlayerIncomeUseCase;
         }
 
         [HttpGet]
@@ -44,6 +50,34 @@ namespace GamingHealth.API.Controllers
             if (result is null)
                 return NotFound(new { message = $"Player {id} não encontrado." });
 
+            return Ok(result);
+        }
+
+        [HttpGet("test-income")]
+        public IActionResult TestIncome()
+        {
+            var player = new Player();
+
+            try
+            {
+                player.UpdateIncome(50000m);
+                return Ok(new { message = "Income atualizado com sucesso.", income = player.Income });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = "Erro ao atualizar o income.", error = ex.Message });
+            }
+        }
+
+        [HttpPatch("{id}/income")]
+        public async Task<IActionResult> UpdateIncome(int id, [FromBody] UpdateIncomeRequest request)
+        {
+            var input = new UpdatePlayerIncomeInput
+            {
+                PlayerId = id,
+                Income = request.Income
+            };
+            var result = await _updatePlayerIncomeUseCase.ExecuteAsync(input);
             return Ok(result);
         }
     }
